c10b8715ea078b341fd7121658ea473aa24e9225
Wyatt C <wcastellan@yahoo.com>
2021-12-17 10:35:07 -0500

    fill in tests and lib files

---
+++ __tests__/manager.test.js
@@ -0,0 +1,15 @@
+const Manager = require('../lib/manager');
+
+jest.mock('../lib/manager');
+
+test('creates manager object', () => {
+    const manager = new Manager('Eve', 32, 'eve@gmail.com', 4);
+
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+test('get role of employee', () => {
+    const manager = new manager('Eve', 32, 'eve@gmail.com');
+
+    expect(manager.getRole().toEqual('Manager'));
+});
